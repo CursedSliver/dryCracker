609720a08ee23124196808afc1b7583f38a51820
CursedSliver <1082sodyx@gmail.com>
2025-05-26 23:24:55 +0800

    hopefully this works

--- dataLoader.js
+++ dataLoader.js
@@ -1,20 +1,36 @@
+let dataLoadSimultaneous = 4; // Number of simultaneous data loads
 async function loadTData(str, destination, box) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     if (loadStatuses[destination] > 0) {
         return;
     }
     loadStatuses[destination]++;
-    for (let i of alphabet) {
-        try {
-            document.getElementById(box).value = 'Fetching data... (' + (alphabet.indexOf(i) / 26 * 100).toFixed(0) + '%)';
-            await fetch('./dataFiles/' + str + '/' + i + 'Data.json').then(res => {
+    for (let i = 0; i < alphabet.length; i += dataLoadSimultaneous) {
+        const promises = [];
+        for (let j = 0; j < dataLoadSimultaneous && (i + j) < alphabet.length; j++) {
+            const letter = alphabet[i + j];
+            const url = `./dataFiles/${str}/${letter}Data.json`;
+            promises.push(
+            fetch(url)
+                .then(res => {
                 if (!res.ok) throw new Error(res.statusText);
                 return res.json();
-            })
-            .then(data => { tData[destination][i] = data; loadStatuses[destination]++; });
+                })
+                .then(data => { 
+                tData[destination][letter] = data; 
+                loadStatuses[destination]++; 
+                })
+                .catch(err => {
+                document.getElementById(box).textContent = 'Failed to fetch data!';
+                throw err;
+                })
+            );
+        }
+        document.getElementById(box).value = 'Fetching data... (' + (i / 26 * 100).toFixed(0) + '%)';
+        try {
+            await Promise.all(promises);
         } catch (err) {
-            document.getElementById(box).textContent = 'Failed to fetch data!';
-            break; 
+            break;
         }
     }
 }
