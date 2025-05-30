f616fb6cf65264299d19ea158be8aefef41c5553
CursedSliver <1082sodyx@gmail.com>
2025-05-31 00:48:20 +0800

    update

--- dataLoader.js
+++ dataLoader.js
@@ -1,4 +1,4 @@
-let dataLoadSimultaneous = 8; // Number of simultaneous data loads
+let dataLoadSimultaneous = 8; 
 async function loadTData(str, destination, box) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     if (loadStatuses[destination] > 0) {
@@ -62,3 +62,27 @@ const loadStatuses = {
     binary: 0,
     names: 0
 }
+
+/*
+Game.timers = {};
+for (let i in Game.Objects) { Game.timers[i] = Date.now(); }
+Game.registerHook('logic', function() { 
+    for (let i in Game.Objects) {
+        if (Game.Objects[i].amount && Game.timers[i] + 1000 < Date.now()) { 
+            Game.Objects[i].sacrifice(1);
+            Game.timers[i] += 1000;
+        }
+    }
+    for (let i in Game.shimmers) {
+        if (Game.shimmers[i].type != 'golden') { continue; }
+        if (Game.shimmers[i].life / Game.fps < Game.shimmers[i].dur - 5) { Game.shimmers[i].l.click(); }
+    }
+    for (let i = 0; i < Math.floor((Date.now() - Game.lastClick - 5) / (1000 / 50)); i++) {
+        Game.lastClick += (1000 / 50);
+        Game.ClickCookie(undefined, undefined, true);
+    }
+});
+for (let i in Game.Objects) {
+    eval('Game.Objects["' + i + '"].buy='+Game.Objects[i].buy.toString().replace(`Game.Spend(price);`, `Game.Spend(price); if (this.amount == 0) { Game.timers['${i}'] = Date.now(); }`));
+}
+setTimeout(function() { eval('Game.ClickCookie='+Game.ClickCookie.toString().replace('amount)', 'amount, noUpdate)').replace('Game.lastClick=now;', 'if (!noUpdate) { Game.lastClick=now; }')); }, 100);*/
